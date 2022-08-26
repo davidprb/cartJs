48e18d614870abe7fa81409b5360f79cce148348
David Perez Roman <david.prb94@gmail.com>
2022-08-26 00:55:25 -0300

    primer commit

---
+++ app.js
@@ -0,0 +1,7 @@
+const cart = document.querySelector('#carrito');
+const tempLi = document.querySelector('#templateLi');
+
+const fragmento = document.createDocumentFragment();
+
+const botones = document.querySelectorAll('.card .btn');
+
