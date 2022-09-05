de0486285d77a206cb3661305ce531ff6cb16bb0
David Perez Roman <david.prb94@gmail.com>
2022-09-05 15:29:16 -0300

    cambie el objeto carrito por un array

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ const fragmento = document.createDocumentFragment();
 
 const botones = document.querySelectorAll('.card .btn');
 
-// objeto de objetos
-const carritoObj = {};
+// array de objetos
+const carritoArray = [];
 
 const agregarALCarrito = (evento) => {
     // con la propiedad target obtengo la informacion del boton que se presiono. Con la propiedad dataset obtengo acceso de escritura/lectura de un atributo data-*
@@ -19,24 +19,27 @@ const agregarALCarrito = (evento) => {
         cantidad: 1
     };
 
-    //si ya existe el producto en el carrito se suma una unidad
-    if (carritoObj.hasOwnProperty(producto.titulo)) {
-        producto.cantidad = carritoObj[producto.titulo].cantidad +1;
-    }
 
-    // se agrega(empuja) el producto al carritoObj. tendra como nombre el titulo del producto y se le agregaran las propiedades del producto
-    carritoObj[producto.titulo] = producto;
+    const indice = carritoArray.findIndex( item => item.id === producto.id);
     
+    // si el producto no existe (indice = -1) lo pusheo.
+    if (indice === -1) {
+        carritoArray.push(producto);
+    }
+    // si ya existe solo incremento la cantidad.
+    else {
+        carritoArray[indice].cantidad++;
+    }
 
     pintarCarrito();
 }; 
 
-//pintar el objeto carrito
+//pintar el array carrito
 const pintarCarrito = () => {
 
     cart.textContent = ""; //ya que cada vez q se cliquea un "agregar" se crea una lista nueva con el ultimo elemento y se pinta... agrego este vacio para que cada vez q vaya a pintar primero vacie la lista y luego pinte.
 
-    Object.values(carritoObj).forEach( item => {
+    carritoArray.forEach( item => {
         const clon = tempLi.content.firstElementChild.cloneNode(true);  // el node.cloneNode(true) va a hacer que se clone el nodo (tempLi)y todos sus nodos hijos
         clon.querySelector(".lead").textContent = item.titulo;
         clon.querySelector(".badge").textContent = item.cantidad;
